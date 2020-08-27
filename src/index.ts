54227eff91e6a8ad84fac71983156b90c4e75634
piraces <raul.piraces@gmail.com>
2020-08-27 19:58:56 +0200

    Add more options

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ const kubeScore = require('./kubescore');
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const ignoreExitCode = core.getInput('ignore-exit-code');
+const ignoreExitCode = core.getInput('ignore-exit-code') as boolean;
 
 async function main() {
     try {
@@ -29,6 +29,7 @@ async function main() {
 
 
 process.on('unhandledRejection', () => {
+    core.info(ignoreExitCode);
     if (ignoreExitCode) {
         core.setFailed('[KUBE-SCORE] Scan failed...');
     } else {
